34003c5bf99b581ce79ed55af571dc836415c99c
AllaM1208 <tionqzmi@gmail.com>
2023-08-10 02:17:39 +0400

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+const categoriesList = document.getElementById("categories");
+
+const categoryItems = categoriesList.querySelectorAll(".item");
+
+console.log(`Number of categories: ${categoryItems.length}`);
+
+categoryItems.forEach(item => {
+    const categoryName = item.querySelector("h2").textContent;
+  
+    const subcategories = item.querySelectorAll("li");
+  
+  console.log(`\nCategory: ${categoryName}`);
+  console.log(`Elements: ${subcategories.length}`);
+});
