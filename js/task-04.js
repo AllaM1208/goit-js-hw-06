a5e56e4439dbfdaa0fbb6f14bb7cea2b570ce480
AllaM1208 <tionqzmi@gmail.com>
2023-08-10 04:17:14 +0400

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,20 @@
+const counterValue = document.querySelector("#value");
+const decrementButton = document.querySelector('button[data-action="decrement"]');
+const incrementButton = document.querySelector('button[data-action="increment"]');
+
+let value = 0;
+
+decrementButton.addEventListener("click", () => {
+  value -= 1;
+  counterValue.textContent = value;
+});
+
+incrementButton.addEventListener("click", () => {
+  value += 1;
+  counterValue.textContent = value;
+});
+
+
+
+
+
